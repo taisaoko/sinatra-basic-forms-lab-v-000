b73ecb80a8be391e08839c400a8ecd8ffcc1935c
taisaoko <ttd3calvin@gmail.com>
2019-05-27 15:25:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
 
     erb :display_puppy
   end
